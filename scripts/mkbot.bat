0245212fdb2ea5960d6c7cb21ca9f7338dca53e5
Taehyeok Kang <taet777@naver.com>
2020-12-26 16:04:34 +0900

    Update scripts

--- scripts/mkbot.bat
+++ scripts/mkbot.bat
@@ -2,7 +2,7 @@
 setlocal
 cd "%~dp0.."
 
-@ If EXIST "build\MKBot.exe" (
+@ If EXIST "build\MKBot-OSS.exe" (
     start build\MKBot-OSS.exe --debug
 )
 endlocal
--- scripts/prepare.bat
+++ scripts/prepare.bat
@@ -1,8 +1,6 @@
 @echo off
 setlocal
 cd "%~dp0.."
-@ If NOT EXIST "build\MKBot.exe" (
-    xcopy /I /Y /E package\data src\data
-)
+xcopy /I /Y /E package\data src\data
 xcopy /I /Y /E package\info src\info
 endlocal
