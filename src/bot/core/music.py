13d29d8aefdcef94abc9a2383f13407f819008af
Michael Kwon <minhyungk72@gmail.com>
2020-12-29 20:31:41 +0630

    music play functioning. Bugs existing

--- src/bot/core/music.py
+++ src/bot/core/music.py
@@ -44,21 +44,21 @@ class Music(commands.Cog):
         if ctx.voice_client == None:
             if ctx.author.voice:
                 await ctx.author.voice.channel.connect()
-                self.playMusic(ctx, song_list[self.songQueue])
+                await self.playMusic(ctx)
             else:
                 await ctx.send(embed=MsgFormatter.get(ctx, "Usage Error", "You are not in any voice channel. Please join a voice channel to use Music bot."))
                 raise commands.CommandError(
                     "Author not connected to a voice channel.")
 
-        while True:
-            if not ctx.voice_client.is_playing:
-                try:
-                    self.songQueue += 1
-                    self.playMusic(ctx, song_list[self.songQueue])
-                except IndexError:
-                    print("end of song Queue")
+    async def playMusic(self, ctx):
+        def next():
+            if len(song_list) > self.songQueue:
+                self.songQueue += 1
+                self.playMusic(ctx)
 
-    def playMusic(self, ctx, song):
+        await ctx.send(embed=MsgFormatter.get(ctx, "Now Playing",
+                                              song_list[self.songQueue].title + "  " + song_list[self.songQueue].length))
+
         ydl_opts = {
             'format': 'bestaudio/best',
             'postprocessors': [{
@@ -68,19 +68,30 @@ class Music(commands.Cog):
             }]
         }
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(song.url, download=False)
+            info = ydl.extract_info(
+                song_list[self.songQueue].url, download=False)
             musicFile = info['formats'][0]['url']
         FFMPEG_OPTIONS = {
             'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-        ctx.voice_client.play(discord.FFmpegPCMAudio(
-            musicFile, **FFMPEG_OPTIONS))
+        try:
+            ctx.voice_client.play(discord.FFmpegPCMAudio(
+                musicFile, **FFMPEG_OPTIONS), after=lambda e: next())
+        except discord.errors.ClientException:
+            pass
 
     @commands.command(aliases=['p'])
     @MGCertificate.verify(level=Level.TRUSTED_USERS)
     async def play(self, ctx: commands.Context, song=''):
         if song == '':
-            ctx.voice_client.resume()
-            await ctx.send(embed=MsgFormatter.get(ctx, 'Player Resumed', 'Now playing: ' + song_list[self.songQueue].title))
+            if ctx.voice_client.is_playing:
+                await ctx.send(embed=MsgFormatter.get(ctx, 'Already Playing', 'The player is already playing ' + song_list[self.songQueue].title))
+            elif ctx.voice_client.is_paused():
+                ctx.voice_client.resume()
+                await ctx.send(embed=MsgFormatter.get(ctx, 'Player Resumed', 'Now playing: ' + song_list[self.songQueue].title))
+            elif len(song_list) == self.songQueue:
+                await ctx.send(embed=MsgFormatter.get(ctx, 'No Song in Queue', 'The player doesn\'t have any song to play. Use //search to add songs in queue'))
+            else:
+                await self.playMusic(ctx)
         # else: #adapt search
 
     @commands.command(aliases=['pp'])
@@ -92,20 +103,36 @@ class Music(commands.Cog):
         else:
             await ctx.send(embed=MsgFormatter.get(ctx, 'Player Already Paused', 'Paused at: ' + song_list[self.songQueue].title))
 
+    @commands.command(aliases=['ss'])
+    @MGCertificate.verify(level=Level.TRUSTED_USERS)
+    async def skip(self, ctx: commands.Context):
+        ctx.voice_client.stop()
+        await ctx.send(embed=MsgFormatter.get(ctx, 'Song Skipped', 'Skipped ' + song_list[self.songQueue - 1].title))
+        await self.playMusic(ctx)
+
+    @MGCertificate.verify(level=Level.TRUSTED_USERS)
+    async def stop(self, ctx: commands.Context):
+        ctx.voice_client.stop()
+        await ctx.send(embed=MsgFormatter.get(ctx, 'Player Stopped'))
+
     @commands.command(aliases=['q'])
     @MGCertificate.verify(level=Level.TRUSTED_USERS)
     async def queue(self, ctx: commands.Context):
         message = ''
-        for i in song_list:
-            message = i.title + ' ' + i.length + '\n'
+        for i in range(len(song_list) - self.songQueue):
+            message += str(i + 1) + '. ' + \
+                song_list[i + self.songQueue].title + ' - ' + \
+                song_list[i + self.songQueue].length + '\n'
         await ctx.send(embed=MsgFormatter.get(ctx, 'Song Queue', message))
 
     @commands.command(aliases=['s'])
     @MGCertificate.verify(level=Level.TRUSTED_USERS)
-    async def search(self, ctx: commands.Context, song):
+    async def search(self, ctx: commands.Context, *song):
         """
         Music command
         """
+        song = " ".join(song)
+
         if "youtube.com" in song:
             r = requests.get(song)
             soup = BeautifulSoup(r.text, 'lxml')
@@ -124,17 +151,19 @@ class Music(commands.Cog):
             s = json.loads(J)
 
             a, b = 0, 0
+            search_song_list = list()
             while True:
                 content = s['contents']['twoColumnSearchResultsRenderer']['primaryContents'][
                     'sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents']
                 if ('videoRenderer' in content[a]) and (not 'badges' in content[a]['videoRenderer']):
                     song_ = Song()
                     song_.searchSong(content, a)
-                    song_list.append(song_)
+                    search_song_list.append(song_)
                     b += 1
                 if b == 4:
                     break
                 a += 1
+            song_list.append(search_song_list[0])  # to be edited
             await ctx.message.delete()
             # to be worked on
             await ctx.send(embed=MsgFormatter.get(ctx, song + ' searched', song_list[-1].title + '\n Length: ' + song_list[-1].length))
