cd3adc03d47d217db51bc7effc571fcc922f72cd
Mycroft Kang <thkang0629@gmail.com>
2023-01-04 00:56:40 +0900

    Add help command in local shell

--- src/bot/shell_host.py
+++ src/bot/shell_host.py
@@ -1,8 +1,18 @@
+import webbrowser
+
 from MGBotBuilder import CommandConsole
 
 from core.controllers.shell.account_controller import account_controller
 from core.controllers.shell.music_controller import music_controller
 
 shell = CommandConsole()
+
+
+@shell.command("/help")
+def show_help(ctx):
+    webbrowser.open("https://github.com/mgylabs/mkbot/wiki/Local-Shell-User-Guide")
+    ctx.send("Open in browser...")
+
+
 shell.extend(music_controller)
 shell.extend(account_controller)
