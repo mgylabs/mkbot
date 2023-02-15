82f7cb59425dce86e2ddb03a552f841847db06bc
Mycroft Kang <thkang0629@gmail.com>
2023-02-15 14:05:26 +0900

    Update hook-mkbot_nlu

--- src/hooks/hook-mkbot_nlu.py
+++ src/hooks/hook-mkbot_nlu.py
@@ -5,3 +5,5 @@ hiddenimports = ["mecab", "mecab_ko_dic"]
 datas = collect_data_files("mkbot_nlu")
 datas += collect_data_files("mecab")
 datas += collect_data_files("mecab_ko_dic")
+
+binaries = [("C:/Windows/System32/msvcp140_1.dll", ".")]
