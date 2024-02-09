edaedd4baafd0d857de5f50b2ac5b10d5a2101c8
Michael Kwon <minhyungk72@gmail.com>
2024-02-09 16:59:50 +0900

    initial class style

--- src/bot/core/controllers/discord/yachtDice.py
+++ src/bot/core/controllers/discord/yachtDice.py
@@ -43,5 +43,85 @@ async def yacht(ctx: commands.Context):
     await botmsg.add_reaction("✅")
 
 
+    class Game(user1, user2):
+        rolledDice = []
+        pickedDice = []
+        def playGame():
+            #run n times of turns, and throw dices 3 times (nested loop)
+            #initialize points and send calculated points to GUI in the loop
+            pointDict = {ones: null, twos: null, threes: null} #continued.. search net
+            #send pointDict each time to Points class, 
+            #then if user chooses the point, send to actual pointDict
+            #if stopGame is called, stop the game
+            pass 
+        totalPoints = 0
+        def stopGame():
+            #강제 게임 종료
+            pass 
+        def organizeDice():
+            #organize dice by faces
+            pass
+        def throwDice (numberOfDices):
+            #run dice
+            for i in range(numberOfDices):
+                rand = random.randint(1, 6)
+                self.rolledDice.append(dices[rand])
+            return self.rolledDice
+        #search up how to use args
+        def pickDice(args):
+            return pickedDice
+        def winner():
+            pass
+        
+    class Points(originPointDict):
+        pointDict = dict()
+        def __init__():
+            run()
+        #private?
+        def sum(pickedDice):
+            sum = 0
+            for dice in pickedDice:
+                sum += dice
+            return sum
+
+        def run(pickedDice):
+            #run all calculations that are not finalized
+            while key in originPointDict.keys(): #is it key?
+                if key != null:
+                    pass
+                    #run.. ? 널이 아니면 특정 함수를 실행시키는 방법이 있을까? 리스트 생성?
+            return pointDict
+
+        def calcSingles(pickedDice, singles):
+            #edit pointDict
+            for dice in pickedDice:
+                if dice == 1:
+                    pointDict[singles]+=1
+        
+        def calcFullHouse(pickedDice):
+            if pickedDice[0] == pickedDice[1] and pickedDice[3]==pickedDice[4]:
+                if pickedDice[2]==pickedDice[1] or pickedDice[2]==pickedDice[3]:
+                    pointDict["fullHouse"] = sum(pickedDice)
+            #if not full house, value is already 0
+        
+        def calcFourOfAKind(pickedDice):
+            if pickedDice[0]==pickedDice[1]:
+                if pickedDice[2]==pickedDice[3]:
+                    pointDict["fourOfAKind"] = self.sum(pickedDice)
+            elif pickedDice[4]==[3]:
+                if pickedDice[1]==[2]:
+                    pointDict["fourOfAKind"] = self.sum(pickedDice)
+        
+        #todo: how to differentiate null state of points and 0 
+        #need to display 0 at times to display what points can be inputted.
+        #option 1: use 0s as results in Points dictionary, and use null in playGame dict
+
+
+        
+    class GUI():
+        #use discord panels
+        pass
+
+
 async def setup(bot: commands.Bot):
     bot.add_command(yacht)
