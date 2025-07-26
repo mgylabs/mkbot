40f7fd0c96363a4bacd11b32fd20abf1ea9e7795
Mycroft Kang <thkang0629@gmail.com>
2025-07-26 21:50:42 +0900

    Fix emoji type for gemini_generating to use animated format

--- src/bot/core/controllers/discord/utils/emoji.py
+++ src/bot/core/controllers/discord/utils/emoji.py
@@ -1,5 +1,5 @@
 class Emoji:
     typing = "<a:typing:1073250215974420490>"
     command = "<:command:1201509794897465354>"
-    gemini_generating = "<:gemini_generating:1398610240228032512>"
+    gemini_generating = "<a:gemini_generating:1398610240228032512>"
     google_gemini = "<:google_gemini:1398611276917248180>"
