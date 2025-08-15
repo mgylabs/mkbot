40fff5e0e86114830f110c7d8dc8d5b41184f2bc
Mycroft Kang <thkang0629@gmail.com>
2025-08-15 17:11:46 +0900

    Update locales

--- scripts/gettext.bat
+++ scripts/gettext.bat
@@ -6,6 +6,9 @@ call .venv\Scripts\activate
 
 @ If /i "%1" == "compile" (
     pybabel compile -d locales -D mkbot
+) Else If /i "%1" == "update" (
+    python "tools/pygettext.py" -p locales -d mkbot -v -D --no-location src/bot src/lib
+    pybabel update -i locales/mkbot.pot -d locales -D mkbot
 ) Else (
     python "tools/pygettext.py" -p locales -d mkbot -v -D --no-location src/bot src/lib
     pybabel compile -d locales -D mkbot
