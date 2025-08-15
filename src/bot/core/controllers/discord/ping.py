b758ee5ad0fe403c1e0066415f242e9afff3f430
Mycroft Kang <thkang0629@gmail.com>
2025-08-15 21:55:20 +0900

    Enhance ping command

--- src/bot/core/controllers/discord/ping.py
+++ src/bot/core/controllers/discord/ping.py
@@ -2,7 +2,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-from mgylabs.i18n import __
+from mgylabs.utils.version import VERSION
 
 from .utils.MGCert import Level, MGCertificate
 from .utils.MsgFormat import MsgFormatter
@@ -15,11 +15,34 @@ async def ping(interaction: discord.Interaction):
     """
     Shows the bot's latency.
     """
-    await interaction.response.send_message(
-        embed=MsgFormatter.get(
-            interaction, __("Pong in %sms") % round(interaction.client.latency * 1000)
-        )
+    v = {
+        "name": "Version",
+        "value": str(VERSION),
+        "inline": True,
+    }
+
+    c = {
+        "name": "Commit",
+        "value": VERSION.commit[:7] if VERSION.commit else "N/A",
+        "inline": True,
+    }
+
+    p = {
+        "name": "Ping",
+        "value": "%sms" % round(interaction.client.latency * 1000),
+        "inline": True,
+    }
+
+    embed = MsgFormatter.get(
+        interaction,
+        "Mulgyeol MK Bot",
+        "Copyright © Mulgyeol Labs. All rights reserved.",
+        [v, c, p],
+        thumbnail_url=interaction.client.user.avatar.url,
+        url="https://github.com/mgylabs/mkbot",
     )
+
+    await interaction.response.send_message(embed=embed)
 
 
 async def setup(bot: commands.Bot):
